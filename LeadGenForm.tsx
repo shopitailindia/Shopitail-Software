b58e003dcdf7d93588eb99765c4c3ed9c546ed18
shopitailindia <shopitailindia@gmail.com>
2025-12-15 14:40:25 +0530

    Refactor form submission to use script URL
    Updated form submission to use Google Apps Script URL and send data as JSON.

--- LeadGenForm.tsx
+++ LeadGenForm.tsx
@@ -1,23 +1,28 @@
 const handleSubmit = async (e: React.FormEvent) => {
   e.preventDefault();
 
-  const url =
-    "https://docs.google.com/forms/d/e/1FAIpQLSffx1rSxkR7q_aL2T6F3aD_RSn9TmK64kNhEIeb1xSw_kqJAA/formResponse";
+  const scriptUrl =
+    "https://script.google.com/macros/s/AKfycbxcHeiM1Zs77wZXBJER3NdoBk8BdfYyE0sA3C0wg2vYd43dd2KCRwxpYjFMLeJnJG0d/exec";
 
-  const fd = new FormData();
-
-  fd.append("entry.1636995483", "Test Name");
-  fd.append("entry.1971314036", "9999999999");
-  fd.append("entry.331570127", "test@gmail.com");
-  fd.append("entry.1424047345", "business");
-  fd.append("entry.2052745801", "2");
-  fd.append("entry.1174324715", "test message");
-
-  await fetch(url, {
+  await fetch(scriptUrl, {
     method: "POST",
-    mode: "no-cors",
-    body: fd,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
   });
 
-  alert("Submitted");
+  setSubmitted(true);
+
+  setTimeout(() => {
+    setSubmitted(false);
+    setFormData({
+      name: "",
+      phone: "",
+      email: "",
+      orgType: "",
+      seats: "",
+      message: "",
+    });
+  }, 3000);
 };
